7669da469cb09a5a818d0decdd89b74081ccb69a
Vincibean <andrebessi00@gmail.com>
2020-09-09 21:20:26 +0100

    code refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -39,8 +39,7 @@ main = do
         .~ [ "datadome=KuVut3_YUoTXhjwlLAdY-qzbJiCVSAYU_8WFZvh.-GqirjO1j9JfyYbeQyJNHI67khneJA.SFNWSjGo-CrPfcFd_pYsDYAABEs_Z.Q6D1~; JSESSIONID.3e635b8a=node01gizd1nish1xn7wv7kw1y6j9a12390192.node0"
            ]
   r <- getWith opts "https://jenkins.otrl.io/crumbIssuer/api/json"
-  let crumb :: Text
-      crumb = r ^. responseBody . key "crumb" . _String
+  let crumb = r ^. responseBody . key "crumb" . _String
   r' <- postWith
     opts
     "https://jenkins.otrl.io/job/sniffles-destroy/buildWithParameters"
