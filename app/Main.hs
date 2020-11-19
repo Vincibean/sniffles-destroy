c623fec3be09550837ce3924f8043976d9093a57
Vincibean <sepensi@gmail.com>
2020-11-19 11:24:09 +0000

    handling multiple boxes

--- app/Main.hs
+++ app/Main.hs
@@ -13,37 +13,32 @@ import           System.Console.CmdArgs         ( Data
                                                 , verbosity
                                                 , Default(def)
                                                 )
-import           Data.ByteString                ( ByteString )
+import qualified Data.ByteString.Lazy.Internal as L
 import qualified Data.ByteString.Char8         as B
 
 import           Network.Wreq                   ( basicAuth
-                                                , getWith
-                                                , postWith
                                                 , defaults
                                                 , auth
-                                                , header
-                                                , manager
                                                 , responseBody
+                                                , Response
                                                 , FormParam((:=))
+                                                , Options
                                                 )
 import qualified Network.Wreq.Session          as S
 import           Control.Lens                   ( (&)
                                                 , (^.)
-                                                , (.~)
                                                 , (?~)
                                                 )
 import           Data.Aeson.Lens                ( key
                                                 , AsPrimitive(_String)
                                                 )
-import           Network.HTTP.Client.TLS        ( mkManagerSettings
-                                                , tlsManagerSettings
-                                                )
+import           Network.HTTP.Client.TLS        ( mkManagerSettings )
 import           Network.Connection             ( TLSSettings(..) )
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import qualified Network.HTTP.Client           as HTTP
 
-data Jenkins = Jenkins { username :: String, password :: String , sidbox :: String } deriving (Data,Typeable,Show,Eq)
+data Jenkins = Jenkins { username :: String, password :: String , sidboxes :: [String] } deriving (Data,Typeable,Show,Eq)
 
 jenkins :: Jenkins
 jenkins =
@@ -54,7 +49,7 @@ jenkins =
       , password =
         def &= help "The password to use to connect to Jenkins" &= typ
           "PASSWORD"
-      , sidbox   = def &= help "The name of the sidbox to destroy" &= typ
+      , sidboxes = def &= help "The name of the sidbox to destroy" &= typ
                      "CORE-1234"
       }
     &= verbosity
@@ -67,25 +62,28 @@ main = do
   sess   <- S.newSessionControl
     (Just (HTTP.createCookieJar []))
     (mkManagerSettings (TLSSettingsSimple True False False) Nothing)
-  let usr  = B.pack $ username params
-  let psw  = B.pack $ password params
-  let box  = sidbox params
-  let opts = defaults & auth ?~ basicAuth usr psw
+  let usr   = B.pack $ username params
+  let psw   = B.pack $ password params
+  let boxes = sidboxes params
+  let opts  = defaults & auth ?~ basicAuth usr psw
   r <- S.getWith opts sess "https://jenkins.otrl.io/crumbIssuer/api/json"
   let crumb = r ^. responseBody . key "crumb" . _String
-  r' <- S.postWith
-    opts
-    sess
-    "https://jenkins.otrl.io/job/sniffles-destroy/build"
-    [ "name" := T.pack "BUILD_NAME"
-    , "value" := T.pack box
-    , "Jenkins-Crumb" := crumb
-    , "json" := T.pack
-      (  "{'parameter':{'name':'BUILD_NAME','value':'"
-      <> box
-      <> "'},'statusCode':'303','redirectTo':'.','Jenkins-Crumb':'"
-      <> T.unpack crumb
-      <> "'}"
-      )
-    ]
+  r' <- destroy opts sess crumb `traverse` boxes
   print r'
+
+destroy :: Options -> S.Session -> Text -> String -> IO (Response L.ByteString)
+destroy opts sess crumb box = S.postWith
+  opts
+  sess
+  "https://jenkins.otrl.io/job/sniffles-destroy/build"
+  [ "name" := T.pack "BUILD_NAME"
+  , "value" := T.pack box
+  , "Jenkins-Crumb" := crumb
+  , "json" := T.pack
+    (  "{'parameter':{'name':'BUILD_NAME','value':'"
+    <> box
+    <> "'},'statusCode':'303','redirectTo':'.','Jenkins-Crumb':'"
+    <> T.unpack crumb
+    <> "'}"
+    )
+  ]
